2d88b9ee35e3db04b50ae8c2465847ad86bae971
yhlee2 <yhlee2@rsupport.com>
2022-11-20 13:59:52 +0900

    필요 클래스 생성

---
+++ app/src/main/java/com/example/webrtc/SignalListener.kt
@@ -0,0 +1,4 @@
+package com.example.webrtc
+
+interface SignalListener {
+}
---
+++ app/src/main/java/com/example/webrtc/WebRTCConnectActivity.kt
@@ -0,0 +1,11 @@
+package com.example.webrtc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WebRTCConnectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_rtcconnect)
+    }
+}
