494ce192050741f2df0842620061c7419c199d1c
yhlee2 <yhlee2@rsupport.com>
2022-11-20 14:47:30 +0900

    WebRTCConnectActivity SignalingClient, SignalListener WebRTCClient, PeerConnectionObserver 초기화

--- app/src/main/java/com/example/webrtc/WebRTCConnectActivity.kt
+++ app/src/main/java/com/example/webrtc/WebRTCConnectActivity.kt
@@ -10,12 +10,18 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.webrtc.databinding.ActivityWebRtcconnectBinding
+import org.webrtc.*
 
 class WebRTCConnectActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWebRtcconnectBinding
 
     private var roomID: String = "test-call"
     private var isJoin: Boolean = false
+
+    private lateinit var rtcClient: WebRTCClient
+    private val signalingClient: SignalingClient by lazy {
+        initializeSignalingClient()
+    }
 
     companion object {
         private const val TAG = "RTCActivity"
@@ -52,9 +58,91 @@ class WebRTCConnectActivity : AppCompatActivity() {
             initializeClient()
         }
     }
+
     private fun initializeClient() {
+        initializeRTCClient()
+        initializeSignalingClient()
+    }
+
+    private fun initializeSignalingClient() = SignalingClient(roomID, createSignalListener())
+
+    private fun initializeRTCClient() {
+        rtcClient = WebRTCClient(application, createPeerConnectionObserver())
+        rtcClient.initSurfaceView(binding.remoteView)
+        rtcClient.initSurfaceView(binding.localView)
+        rtcClient.startLocalView(binding.localView)
+        Log.e("ROOMID", "$roomID")
+        if (!isJoin) rtcClient.call(roomID)
+    }
+
+    private fun createSignalListener(): SignalListener = object : SignalListener {
+        override fun onOfferReceived(description: SessionDescription) {
+            if (isJoin) {
+                rtcClient.onRemoteSessionReceived(description)
+                rtcClient.answer(roomID)
+            }
+        }
+
+        override fun onAnswerReceived(description: SessionDescription) {
+            if (isJoin.not()) rtcClient.onRemoteSessionReceived(description)
+        }
+
+        override fun onIceCandidateReceived(iceCandidate: IceCandidate) {
+            rtcClient.addCandidate(iceCandidate)
+        }
 
     }
+
+    private fun createPeerConnectionObserver(): PeerConnection.Observer =
+        object : PeerConnection.Observer {
+            override fun onSignalingChange(p0: PeerConnection.SignalingState?) {
+                Log.e("Rsupport", "onSignalingChange")
+            }
+
+            override fun onIceConnectionChange(p0: PeerConnection.IceConnectionState?) {
+                Log.e("Rsupport", "onIceConnectionChange")
+            }
+
+            override fun onIceConnectionReceivingChange(p0: Boolean) {
+                Log.e("Rsupport", "onIceConnectionReceivingChange")
+            }
+
+            override fun onIceGatheringChange(p0: PeerConnection.IceGatheringState?) {
+                Log.e("Rsupport", "onIceGatheringChange")
+            }
+
+            override fun onIceCandidate(p0: IceCandidate?) {
+                Log.e("Rsupport", "onIceCandidate : $p0")
+                signalingClient.sendIceCandidate(p0, isJoin)
+                rtcClient.addCandidate(p0)
+            }
+
+            override fun onIceCandidatesRemoved(p0: Array<out IceCandidate>?) {
+                Log.e("Rsupport", "onIceCandidatesRemoved")
+            }
+
+            override fun onAddStream(p0: MediaStream?) {
+                Log.e("Rsupport", "onAddStream")
+                p0?.videoTracks?.get(0)?.addSink(binding.remoteView)
+            }
+
+            override fun onRemoveStream(p0: MediaStream?) {
+                Log.e("Rsupport", "onRemoveStream")
+            }
+
+            override fun onDataChannel(p0: DataChannel?) {
+                Log.e("Rsupport", "onDataChannel")
+            }
+
+            override fun onRenegotiationNeeded() {
+                Log.e("Rsupport", "onRenegotiationNeeded")
+            }
+
+            override fun onAddTrack(p0: RtpReceiver?, p1: Array<out MediaStream>?) {
+                Log.e("Rsupport", "onAddTrack")
+            }
+
+        }
 
     private fun requestCameraAndAudioPermission(dialogShown: Boolean = false) {
         Log.e(TAG, "RequestPermission")
