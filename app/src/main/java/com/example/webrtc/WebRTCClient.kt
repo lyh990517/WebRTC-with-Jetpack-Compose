e0bca3b501eba0550fd452dc1ae1fd1b00c23ff2
yhlee2 <yhlee2@rsupport.com>
2022-11-20 14:27:24 +0900

    WebRTCClient 뼈대 생성

--- app/src/main/java/com/example/webrtc/WebRTCClient.kt
+++ app/src/main/java/com/example/webrtc/WebRTCClient.kt
@@ -1,4 +1,33 @@
 package com.example.webrtc
 
-class WebRTCClient {
+import android.app.Application
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import org.webrtc.*
+
+class WebRTCClient(
+    context: Application,
+    private val observer: PeerConnection.Observer
+) {
+    companion object {
+        private const val LOCAL_TRACK_ID = "local_track"
+        private const val LOCAL_STREAM_ID = "local_track"
+        private const val FIELD_TRIALS = "WebRTC-H264HighProfile/Enabled/"
+        private const val ICE_SERVER_URL = "stun:stun.l.google.com:19302"
+    }
+    private val constraints = MediaConstraints().apply {
+        mandatory.add(MediaConstraints.KeyValuePair("OfferToReceiveVideo", "true"))
+    }
+
+    private val rootEglBase: EglBase = EglBase.create()
+
+    private var localAudioTrack: AudioTrack? = null
+    private var localVideoTrack: VideoTrack? = null
+
+    private val database = Firebase.firestore
+
+    init {
+
+    }
+
 }
