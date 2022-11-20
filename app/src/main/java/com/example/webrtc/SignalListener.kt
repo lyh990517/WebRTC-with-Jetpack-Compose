a23bba1dce499a2224c8589c1e1dd2954c307bb5
yhlee2 <yhlee2@rsupport.com>
2022-11-20 14:01:16 +0900

    SignalListener 인터페이스 정의

--- app/src/main/java/com/example/webrtc/SignalListener.kt
+++ app/src/main/java/com/example/webrtc/SignalListener.kt
@@ -1,4 +1,10 @@
 package com.example.webrtc
 
+import org.webrtc.IceCandidate
+import org.webrtc.SessionDescription
+
 interface SignalListener {
+    fun onOfferReceived(description: SessionDescription)
+    fun onAnswerReceived(description: SessionDescription)
+    fun onIceCandidateReceived(iceCandidate: IceCandidate)
 }
