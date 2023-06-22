7fd5e02d8fc8273fab8aa16e9a9f63b98b857ebe
yhlee2 <yhlee2@rsupport.com>
2023-06-22 15:19:14 +0900

    코드 정리

--- app/src/main/java/com/example/webrtc/view/WebRTCConnectActivity.kt
+++ app/src/main/java/com/example/webrtc/view/WebRTCConnectActivity.kt
@@ -16,38 +16,39 @@ import com.example.webrtc.databinding.ActivityWebRtcconnectBinding
 import com.example.webrtc.event.PeerConnectionEvent
 import com.example.webrtc.event.SignalEvent
 import com.example.webrtc.event.WebRTCEvent
+import com.example.webrtc.state.UiState
 import com.example.webrtc.viewmodel.ConnectionViewModel
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class WebRTCConnectActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityWebRtcconnectBinding
 
-    @Inject
-    lateinit var signalRepository: SignalRepository
-
-    @Inject
-    lateinit var firestore: FirebaseFirestore
-
     private val viewModel: ConnectionViewModel by viewModels()
-
-    private var roomID: String = "test-call"
-    private var isJoin: Boolean = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWebRtcconnectBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        collectSignalEvent()
         collectConnectionEvent()
         collectRTCEvent()
-        getIntentData()
-        viewModel.initRTC()
+        collectState()
+    }
 
+    private fun collectState() {
+        lifecycleScope.launch {
+            viewModel.uiState.collect{
+                when(it){
+                    is UiState.UnInitialized -> viewModel.initRTC()
+                }
+            }
+        }
     }
 
     private fun collectRTCEvent() {
@@ -55,14 +56,13 @@ class WebRTCConnectActivity : AppCompatActivity() {
             viewModel.webRTCEvent.collect {
                 when (it) {
                     is WebRTCEvent.Initialize -> {
-                        Log.e("rtc","initialize")
                         it.webRTCClient.apply {
                             initPeerConnectionFactory(application)
                             initVideoCapture(application)
                             initSurfaceView(binding.remoteView)
                             initSurfaceView(binding.localView)
                             startLocalView(binding.localView)
-                            if(!isJoin) call(roomID)
+                            viewModel.call()
                             viewModel.connect()
                         }
                     }
@@ -76,50 +76,15 @@ class WebRTCConnectActivity : AppCompatActivity() {
             viewModel.peerConnectionEvent.collect {
                 when (it) {
                     is PeerConnectionEvent.OnIceCandidate -> {
-                        Log.e("peerConnectionEvent","OnIceCandidate")
-                        viewModel.sendIceCandidate(it.data, isJoin, roomID)
+                        viewModel.sendIceCandidate(it.data)
                         viewModel.addCandidate(it.data)
                     }
 
                     is PeerConnectionEvent.OnAddStream -> {
-                        Log.e("peerConnectionEvent","OnAddStream")
                         it.data.videoTracks?.get(0)?.addSink(binding.remoteView)
                     }
                 }
             }
         }
-    }
-
-    private fun collectSignalEvent() {
-        lifecycleScope.launch {
-            viewModel.signalEvent.collect {
-                Log.e("event", "$it")
-                when (it) {
-                    is SignalEvent.OfferReceived -> {
-                        if (isJoin) {
-                            Log.e("signalEvent","OfferReceived")
-                            viewModel.onRemoteSessionReceived(it.data)
-                            viewModel.answer(roomID)
-                        }
-                    }
-
-                    is SignalEvent.AnswerReceived -> {
-                        Log.e("signalEvent","AnswerReceived")
-                        if (isJoin.not()) viewModel.onRemoteSessionReceived(it.data)
-                    }
-
-                    is SignalEvent.IceCandidateReceived -> {
-                        Log.e("signalEvent","IceCandidateReceived")
-                        viewModel.addCandidate(it.data)
-                    }
-                }
-            }
-        }
-    }
-
-    private fun getIntentData() {
-        roomID = intent?.getStringExtra("roomID")!!
-        isJoin = intent?.getBooleanExtra("isJoin", false)!!
-        viewModel.initSignal(roomID)
     }
 }
