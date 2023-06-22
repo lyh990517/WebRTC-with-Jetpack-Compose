b9089532c8ef87d10963b5302bbeb170b1e87f75
yhlee2 <yhlee2@rsupport.com>
2023-06-22 14:50:10 +0900

    권한 요청 화면 변경

--- app/src/main/java/com/example/webrtc/view/WebRTCConnectActivity.kt
+++ app/src/main/java/com/example/webrtc/view/WebRTCConnectActivity.kt
@@ -37,12 +37,6 @@ class WebRTCConnectActivity : AppCompatActivity() {
     private var roomID: String = "test-call"
     private var isJoin: Boolean = false
 
-    companion object {
-        private const val TAG = "RTCActivity"
-        private const val CAMERA_AUDIO_PERMISSION_REQUEST_CODE = 1
-        private const val CAMERA_PERMISSION = Manifest.permission.CAMERA
-        private const val AUDIO_PERMISSION = Manifest.permission.RECORD_AUDIO
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,7 +46,7 @@ class WebRTCConnectActivity : AppCompatActivity() {
         collectConnectionEvent()
         collectRTCEvent()
         getIntentData()
-        checkCameraAndAudioPermission()
+        viewModel.initRTC()
 
     }
 
@@ -127,62 +121,5 @@ class WebRTCConnectActivity : AppCompatActivity() {
         roomID = intent?.getStringExtra("roomID")!!
         isJoin = intent?.getBooleanExtra("isJoin", false)!!
         viewModel.initSignal(roomID)
-    }
-
-    private fun checkCameraAndAudioPermission() {
-        Log.e(TAG, "checkPermission")
-        if (ContextCompat.checkSelfPermission(
-                this,
-                CAMERA_PERMISSION
-            ) != PackageManager.PERMISSION_GRANTED
-            && ContextCompat.checkSelfPermission(
-                this,
-                AUDIO_PERMISSION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            requestCameraAndAudioPermission()
-        } else {
-            viewModel.initRTC()
-        }
-    }
-
-    private fun requestCameraAndAudioPermission(dialogShown: Boolean = false) {
-        Log.e(TAG, "RequestPermission")
-        if (ActivityCompat.shouldShowRequestPermissionRationale(
-                this,
-                CAMERA_PERMISSION
-            ) && ActivityCompat.shouldShowRequestPermissionRationale(
-                this,
-                AUDIO_PERMISSION
-            ) && !dialogShown
-        ) {
-            showPermissionRationDialog()
-        } else {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(CAMERA_PERMISSION, AUDIO_PERMISSION),
-                CAMERA_AUDIO_PERMISSION_REQUEST_CODE
-            )
-        }
-    }
-
-    private fun showPermissionRationDialog() {
-        Log.e(TAG, "showPermissionDialog")
-        AlertDialog.Builder(this).apply {
-            setTitle("Camera And Audio Permission Required")
-            setMessage("This app need the camera and audio to function")
-            setPositiveButton("Grant") { dialog, _ ->
-                dialog.dismiss()
-                requestCameraAndAudioPermission(true)
-            }
-            setNegativeButton("Deny") { dialog, _ ->
-                dialog.dismiss()
-                permissionDenied()
-            }
-        }
-    }
-
-    private fun permissionDenied() {
-        Toast.makeText(this, "Camera and Audio Permission Denied", Toast.LENGTH_LONG).show()
     }
 }
