b94485cc4ae0c00b5b681e713e643a7c594ebecd
lyh990517 <lyh990517@naver.com>
2023-06-27 19:22:30 +0900

    제작자 이름 삽입

--- presentaion/src/main/java/com/example/presentaion/view/MainScreen.kt
+++ presentaion/src/main/java/com/example/presentaion/view/MainScreen.kt
@@ -36,12 +36,18 @@ fun MainScreen(viewModel: FireStoreViewModel) {
     val roomId = remember { mutableStateOf("") }
     Column(
         modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        LogoImage()
-        Spacer(modifier = Modifier.padding(vertical = 50.dp))
-        InputContent(roomId, viewModel)
+        Column(
+            modifier = Modifier.weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            LogoImage()
+            Spacer(modifier = Modifier.padding(vertical = 50.dp))
+            InputContent(roomId, viewModel)
+        }
+        Text(text = "Created by yunho 2023", Modifier.padding(bottom = 10.dp))
     }
 }
 
