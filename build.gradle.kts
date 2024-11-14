c502b5054df576af7d727110e8fac99a5fa6243e
lyh990517 <lyh990517@naver.com>
2024-11-14 10:46:17 +0900

    프로젝트 gradle kotlin 마이그레이션

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("com.android.application") version "7.3.1" apply false
+    id("com.android.library") version "7.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.google.gms.google-services") version "4.3.14" apply false
+    id("com.google.dagger.hilt.android") version "2.45" apply false
+}
