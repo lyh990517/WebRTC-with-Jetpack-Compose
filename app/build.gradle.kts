9d4f1dd13bda9ba7cb52a8fce6e9f69f203a999d
lyh990517 <lyh990517@naver.com>
2024-11-14 23:03:02 +0900

    gradle 정리

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,34 +5,6 @@ plugins {
 
 android {
     namespace = "com.example.webrtc"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.example.webrtc"
-        minSdk = 21
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 }
 
 dependencies {
