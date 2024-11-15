27b18035e15967e0b546ee780b1c0b6a0e66e277
lyh990517 <lyh990517@naver.com>
2024-11-15 16:52:19 +0900

    project gradle 정리

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
-    id("com.android.application") version "8.3.2" apply false
-    id("com.android.library") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("com.google.gms.google-services") version "4.3.2" apply false
-    id("com.google.dagger.hilt.android") version "2.52" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.google.service) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.ksp) apply false
 }
