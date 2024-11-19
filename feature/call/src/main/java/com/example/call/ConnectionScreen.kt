c75c338f7b5ccc6b194837faffce662752fabea1
lyh990517 <lyh990517@naver.com>
2024-11-19 18:21:14 +0900

    디자인 변경

--- feature/call/src/main/java/com/example/call/ConnectionScreen.kt
+++ feature/call/src/main/java/com/example/call/ConnectionScreen.kt
@@ -7,11 +7,12 @@ import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -27,7 +28,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -81,6 +85,7 @@ private fun CallContent(
     onMessage: (String) -> Unit
 ) {
     var isChat by remember { mutableStateOf(false) }
+    var localViewOffset by remember { mutableStateOf(Offset(16f, 16f)) }
 
     Box(modifier = Modifier.fillMaxSize()) {
         AndroidView(
@@ -88,36 +93,40 @@ private fun CallContent(
             factory = { state.remote }
         )
 
-        Column(
-            modifier = Modifier.align(Alignment.BottomCenter)
-        ) {
-            AndroidView(
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .size(100.dp)
-                    .border(2.dp, Color.White, CircleShape)
-                    .padding(16.dp),
-                factory = { state.local }
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(
-                        color = Color.Black.copy(alpha = 0.5f),
-                        shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
-                    )
-                    .padding(vertical = 16.dp, horizontal = 32.dp)
-            ) {
-                ControllerUi(
-                    modifier = Modifier.fillMaxWidth(),
-                    onToggleVoice = onToggleVoice,
-                    onToggleVideo = onToggleVideo,
-                    onToggleChat = { isChat = !isChat },
-                    onDisconnect = onDisconnect,
-                )
-            }
-        }
+        AndroidView(
+            modifier = Modifier
+                .padding(20.dp)
+                .align(Alignment.TopEnd)
+                .size(100.dp)
+                .offset { IntOffset(localViewOffset.x.toInt(), localViewOffset.y.toInt()) }
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+                        localViewOffset += dragAmount
+                    }
+                }
+                .clip(CircleShape),
+            factory = { state.local }
+        )
 
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .background(
+                    color = Color.Black.copy(alpha = 0.5f),
+                    shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
+                )
+                .padding(vertical = 16.dp, horizontal = 32.dp)
+        ) {
+            ControllerUi(
+                modifier = Modifier.fillMaxWidth(),
+                onToggleVoice = onToggleVoice,
+                onToggleVideo = onToggleVideo,
+                onToggleChat = { isChat = !isChat },
+                onDisconnect = onDisconnect,
+            )
+        }
 
         AnimatedVisibility(
             visible = isChat,
@@ -127,7 +136,7 @@ private fun CallContent(
             Chatting(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .fillMaxHeight(0.4f)
+                    .fillMaxHeight()
                     .align(Alignment.BottomCenter)
                     .background(
                         color = Color.White.copy(alpha = 0.95f),
