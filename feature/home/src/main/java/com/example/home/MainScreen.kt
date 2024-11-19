81672ff331259538b39498ca7249a516b95ada83
lyh990517 <lyh990517@naver.com>
2024-11-19 17:56:04 +0900

    권한 요청 변경

--- feature/home/src/main/java/com/example/home/MainScreen.kt
+++ feature/home/src/main/java/com/example/home/MainScreen.kt
@@ -1,5 +1,12 @@
 package com.example.home
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -7,13 +14,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.compose.composable
+import androidx.core.content.ContextCompat
 import com.example.home.ui.InputContent
 import com.example.home.ui.LogoImage
 
@@ -21,7 +30,20 @@ import com.example.home.ui.LogoImage
 fun MainScreen(
     goToCall: (String) -> Unit
 ) {
-    val roomId = remember { mutableStateOf("") }
+    var roomId by remember { mutableStateOf("") }
+    val context = LocalContext.current
+
+    val permissionsLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        val allGranted = permissions.values.all { it }
+        if (allGranted) {
+            Toast.makeText(context, "Permissions Granted", Toast.LENGTH_SHORT).show()
+            goToCall(roomId)
+        } else {
+            Toast.makeText(context, "Camera and Audio Permission Denied", Toast.LENGTH_LONG).show()
+        }
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -35,13 +57,43 @@ fun MainScreen(
             LogoImage()
             Spacer(modifier = Modifier.padding(vertical = 50.dp))
             InputContent(
-                roomId = { roomId.value },
+                roomId = { roomId },
                 onCall = {
-                    goToCall(roomId.value)
+                    checkCameraAndAudioPermission(
+                        context = context,
+                        goToCall = { goToCall(roomId) },
+                        permissionsLauncher = permissionsLauncher
+                    )
                 },
-                onInput = { roomId.value = it }
+                onInput = { roomId = it }
             )
         }
         Text(text = "Created by yunho 2023", Modifier.padding(bottom = 10.dp))
+    }
+}
+
+fun checkCameraAndAudioPermission(
+    context: Context,
+    goToCall: () -> Unit,
+    permissionsLauncher: ManagedActivityResultLauncher<Array<String>, Map<String, @JvmSuppressWildcards Boolean>>
+) {
+    val cameraGranted = ContextCompat.checkSelfPermission(
+        context,
+        Manifest.permission.CAMERA
+    ) == PackageManager.PERMISSION_GRANTED
+    val audioGranted = ContextCompat.checkSelfPermission(
+        context,
+        Manifest.permission.RECORD_AUDIO
+    ) == PackageManager.PERMISSION_GRANTED
+
+    if (!cameraGranted || !audioGranted) {
+        permissionsLauncher.launch(
+            arrayOf(
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
+            )
+        )
+    } else {
+        goToCall()
     }
 }
--- feature/main/src/main/java/com/example/main/MainActivity.kt
+++ feature/main/src/main/java/com/example/main/MainActivity.kt
@@ -1,92 +1,19 @@
 package com.example.main
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        checkCameraAndAudioPermission()
         enableEdgeToEdge()
 
         setContent {
             AppGraph()
         }
-    }
-
-    private fun checkCameraAndAudioPermission() {
-        Log.e(TAG, "checkPermission")
-        if (ContextCompat.checkSelfPermission(
-                this,
-                CAMERA_PERMISSION
-            ) != PackageManager.PERMISSION_GRANTED
-            && ContextCompat.checkSelfPermission(
-                this,
-                AUDIO_PERMISSION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            requestCameraAndAudioPermission()
-        }
-    }
-
-    private fun requestCameraAndAudioPermission(dialogShown: Boolean = false) {
-        Log.e(TAG, "RequestPermission")
-        if (ActivityCompat.shouldShowRequestPermissionRationale(
-                this,
-                CAMERA_PERMISSION
-            ) && ActivityCompat.shouldShowRequestPermissionRationale(
-                this,
-                AUDIO_PERMISSION
-            ) && !dialogShown
-        ) {
-            showPermissionRationDialog()
-        } else {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(
-                    CAMERA_PERMISSION,
-                    AUDIO_PERMISSION
-                ),
-                CAMERA_AUDIO_PERMISSION_REQUEST_CODE
-            )
-        }
-    }
-
-    private fun showPermissionRationDialog() {
-        Log.e(TAG, "showPermissionDialog")
-        AlertDialog.Builder(this).apply {
-            setTitle("Camera And Audio Permission Required")
-            setMessage("This app need the camera and audio to function")
-            setPositiveButton("Grant") { dialog, _ ->
-                dialog.dismiss()
-                requestCameraAndAudioPermission(true)
-            }
-            setNegativeButton("Deny") { dialog, _ ->
-                dialog.dismiss()
-                permissionDenied()
-            }
-        }
-    }
-
-    private fun permissionDenied() {
-        Toast.makeText(this, "Camera and Audio Permission Denied", Toast.LENGTH_LONG).show()
-    }
-
-    companion object {
-        private const val TAG = "RTCActivity"
-        private const val CAMERA_AUDIO_PERMISSION_REQUEST_CODE = 1
-        private const val CAMERA_PERMISSION = Manifest.permission.CAMERA
-        private const val AUDIO_PERMISSION = Manifest.permission.RECORD_AUDIO
     }
 }
