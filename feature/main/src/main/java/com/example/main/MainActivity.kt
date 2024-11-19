c44d9dfe506b2ccb64d5c86f5e3d031db2bbe038
lyh990517 <lyh990517@naver.com>
2024-11-19 23:19:09 +0900

    component activity 로 변경

--- feature/main/src/main/java/com/example/main/MainActivity.kt
+++ feature/main/src/main/java/com/example/main/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.main
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
