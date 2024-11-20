fcccda7ab7a47c00b86ad25eff0b5223685e0559
lyh990517 <lyh990517@naver.com>
2024-11-20 16:44:09 +0900

    버튼 디자인 통일

--- feature/home/src/main/java/com/example/home/MainScreen.kt
+++ feature/home/src/main/java/com/example/home/MainScreen.kt
@@ -22,23 +22,24 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.AlertDialog
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -52,12 +53,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.home.ui.InputContent
 import com.example.home.ui.LogoImage
 
 @Composable
@@ -92,21 +92,59 @@ fun MainScreen(
     ) {
         LogoImage()
         Spacer(modifier = Modifier.height(40.dp))
-        InputContent(
-            roomId = { roomId },
-            onInput = { roomId = it },
-            onCall = {
-                checkCameraAndAudioPermission(
-                    context = context,
-                    goToCall = { goToCall(roomId) },
-                    permissionsLauncher = permissionsLauncher
-                )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = roomId,
+                onValueChange = { it: String -> roomId = it },
+                label = { Text(text = "Room Name") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 20.dp, bottom = 24.dp)
+            )
+            Button(
+                onClick = {
+                    checkCameraAndAudioPermission(
+                        context = context,
+                        goToCall = { goToCall(roomId) },
+                        permissionsLauncher = permissionsLauncher
+                    )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                shape = RoundedCornerShape(50),
+                colors = ButtonDefaults.buttonColors(Color(0xFF6200EE)),
+                elevation = ButtonDefaults.elevatedButtonElevation(8.dp)
+            ) {
+                Text(text = "Create Room", color = Color.White, fontSize = 18.sp)
             }
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { isDialogVisible = true }) {
-            Text("Show Rooms")
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { isDialogVisible = true },
+            modifier = Modifier
+                .height(50.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp),
+            shape = RoundedCornerShape(50),
+            colors = ButtonDefaults.buttonColors(Color(0xFF6200EE)),
+            elevation = ButtonDefaults.elevatedButtonElevation(8.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Show Rooms", color = Color.White, fontSize = 18.sp)
+            }
+        }
+
         Spacer(modifier = Modifier.weight(1f))
         Text(
             text = "Created by yunho 2023",
@@ -139,7 +177,10 @@ fun RoomDialog(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black.copy(alpha = 0.5f))
-            .clickable(onClick = onDismiss, indication = null, interactionSource = remember { MutableInteractionSource() })
+            .clickable(
+                onClick = onDismiss,
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() })
     ) {
         Card(
             modifier = Modifier
@@ -201,7 +242,10 @@ fun RoomDialog(
                     horizontalArrangement = Arrangement.End
                 ) {
                     TextButton(onClick = onDismiss) {
-                        Text("Close", style = MaterialTheme.typography.bodyLarge.copy(color = Color(0xFF6200EA)))
+                        Text(
+                            "Close",
+                            style = MaterialTheme.typography.bodyLarge.copy(color = Color(0xFF6200EA))
+                        )
                     }
                 }
             }
